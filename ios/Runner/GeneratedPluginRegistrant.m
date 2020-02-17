2478367fbd3c7217081ba83358e291ed926e61b5
josiasp <pina.jisrael@gmail.com>
2020-02-17 16:34:57 -0500

    test

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,30 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <connectivity/ConnectivityPlugin.h>
+#import <flutter_appavailability/AppAvailability.h>
+#import <fluttertoast/FluttertoastPlugin.h>
+#import <google_maps_flutter/GoogleMapsPlugin.h>
+#import <location/LocationPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+#import <shared_preferences/SharedPreferencesPlugin.h>
+#import <sqflite/SqflitePlugin.h>
+#import <url_launcher/UrlLauncherPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTConnectivityPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTConnectivityPlugin"]];
+  [AppAvailability registerWithRegistrar:[registry registrarForPlugin:@"AppAvailability"]];
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [FLTGoogleMapsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleMapsPlugin"]];
+  [LocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocationPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
+}
+
+@end
